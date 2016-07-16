57dc1a0e350530a5c5c1d43a61c6b3b56899f2ac
Ben Ricker <BGRicker@gmail.com>
2016-07-16 16:42:39 -0400

    capybara, static page test

---
+++ spec/features/static_pages_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, :type => :controller do
+
+  describe "GET index" do
+
+    it "renders the :index template" do
+      expect(get: root_url(subdomain: nil)).to route_to(
+        controller: "static_pages",
+        action: "index")
+    end
+
+  end
+
+end
