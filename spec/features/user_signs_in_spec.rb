7af40eb8ec3f00af42b08264ee8393c297f35cf7
Ben Ricker <bgricker@gmail.com>
2016-07-23 10:56:57 -0400

    database cleaner, sign in spec

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -6,6 +6,17 @@ RSpec.describe User, :type => :feature do
 
     it "signs the user up" do
       sign_up_as("Kanye@west.com", "pizza123123")
+      expect(page).to have_content "Welcome! You have signed up successfully."
+    end
+
+    it "won't allow a blank username" do
+      sign_up_as("", "pizza123123")
+      expect(page).to have_content "Email can't be blank"
+    end
+
+    it "won't allow a blank password" do
+      sign_up_as("Cool@gmail.com", "")
+      expect(page).to have_content "Password can't be blank"
     end
   end
 end
