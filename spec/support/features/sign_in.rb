c9301e4b22dab12d0301dc55aa22768a788520fa
Ben Ricker <bgricker@gmail.com>
2016-07-23 00:06:17 -0400

    delete extra specs

--- spec/support/features/sign_in.rb
+++ spec/support/features/sign_in.rb
@@ -8,4 +8,13 @@ module Features
     fill_in "Email", with: email
     click_on  "Sign in"
   end
+
+  def sign_up_as(email, password)
+    visit root_path
+    click_on 'Sign up'
+    fill_in "Email", with: email
+    fill_in "Password", with: password
+    fill_in "Password confirmation", with: password
+    click_on  "Sign up"
+  end
 end
