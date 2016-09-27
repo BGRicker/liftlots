72b2f418f06e4d3376950f471106d74aa8af6ae1
josephferrairo <josephferrairo@gmail.com>
2016-09-26 23:30:44 -0400

    added two failiure tests to exercise controller spec, one for in invalid rep input(0 reps), and one for a blank name field

--- spec/controllers/exercise_controller_spec.rb
+++ spec/controllers/exercise_controller_spec.rb
@@ -9,8 +9,20 @@ RSpec.describe ExercisesController, type: :controller do
   describe "POST #create" do
     context "a successful create" do
       it "saves a new exercise object" do
-        expect { post :create, workout_id: @workout, exercise: @exercise_attributes }.to change( Exercise, :count ).by(1)
+        expect { post :create, workout_id: @workout, exercise: @exercise_attributes }.to change( Exercise, :count).by(1)
       end
+    end
+  end
+
+  context "User leaves name field blank" do
+    it "does not save a new exercise object" do
+      expect { post :create, workout_id: @workout, exercise: {name: '', reps: 12 } }.to change( Exercise, :count).by(0)
+    end
+  end
+
+  context "User inputs 0 in reps" do
+    it "does not save a new exercise object" do
+      expect { post :create, workout_id: @workout, exercise: {name: 'Chest', reps: 0 } }.to change( Exercise, :count).by(0)
     end
   end
 end
