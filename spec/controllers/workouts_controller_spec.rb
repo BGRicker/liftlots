97a193270e8c001eb5a0afe5a6d1e03b16434383
josephferrairo <josephferrairo@gmail.com>
2016-10-07 21:06:14 -0400

    added tests for new and create workout

--- spec/controllers/workouts_controller_spec.rb
+++ spec/controllers/workouts_controller_spec.rb
@@ -9,4 +9,21 @@ RSpec.describe WorkoutsController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+  describe "GET #new" do
+    login_user
+    it "returns a successful http request status code" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "POST #create" do
+    login_user
+    context "a successful create" do
+      it "saves the customer object" do
+
+        expect { post :create, params: { workout: Fabricate.attributes_for(:workout) } }.to change( Workout, :count).by(1)
+      end
+    end
+  end
 end
